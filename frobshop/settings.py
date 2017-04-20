f77c8053bfdc2bdfd9e6182a0dab9035b8412f92
twwu <twwu@twwudeMac-mini.local>
2017-04-20 17:59:53 +0800

    Added accounts

--- frobshop/settings.py
+++ frobshop/settings.py
@@ -106,6 +106,31 @@ TEMPLATES = [
     },
 ]
 
+OSCAR_DASHBOARD_NAVIGATION.append(
+	{
+		'label':'Accounts',
+		'icon':'icon-globe',
+		'children':[
+			{
+				'label':'Accounts',
+				'url_name':'accounts-list',
+			},
+			{
+				'label':'Transfers',
+				'url_name':'transfers-list',
+			},
+			{
+				'label':'Deferred income report',
+				'url_name':'report-deferred-income',
+			},
+			{
+				'label':'Profit/loss report',
+				'url_name':'report-profit-loss',
+			},
+		]
+	}
+)
+
 WSGI_APPLICATION = 'frobshop.wsgi.application'
 
 
--- frobshop/urls.py
+++ frobshop/urls.py
@@ -16,9 +16,11 @@ from django.conf.urls import include, url
 from django.contrib import admin
 from oscar.app import application
 
+from oscar_accounts.dashboard.app import application as accounts_app
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
 	url(r'^i18n/',include('django.conf.urls.i18n')),
 	url(r'',include(application.urls)),
+	url(r'^dashboard/accounts/', include(accounts_app.urls)),
 ]
